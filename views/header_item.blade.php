2cbd3490ae2211e74b317bc950c23633ab0dfc5a
M N Islam Shihan <mnishihan@gmail.com>
2013-05-16 21:50:05 +0600

    Added descriptive title for sort links

--- views/header_item.blade.php
+++ views/header_item.blade.php
@@ -3,12 +3,12 @@
     @if($attribute == Input::query('sort_by',false) && 'ASC' == Input::query('sort_dir','ASC') )
      	&uarr;
     @else
-    {{ HTML::link($sort_url_asc,'↑', array(), Request::secure())}}
+    {{ HTML::link($sort_url_asc,'↑', array('title'=>'Sort Ascending'), Request::secure())}}
     @endif
 
     @if($attribute == Input::query('sort_by',false) && 'DESC' == Input::query('sort_dir','ASC'))
     &darr;
     @else
-    {{ HTML::link($sort_url_desc,'↓', array(), Request::secure())}}
+    {{ HTML::link($sort_url_desc,'↓', array('title'=>'Sort Descending'), Request::secure())}}
     @endif
  @endif
