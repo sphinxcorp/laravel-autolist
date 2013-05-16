40efe47da6a8574ffbbc3fa2743f9954248f5006
M N Islam Shihan <mnishihan@gmail.com>
2013-05-16 20:51:27 +0600

    Replaced sort icons with &uarr; and &darr; entities

--- views/header_item.blade.php
+++ views/header_item.blade.php
@@ -1,14 +1,14 @@
 <span class="{{ $sortable?"sortable":"" }}">{{ $title }}</span> 
  @if($sortable)
     @if($attribute == Input::query('sort_by',false) && 'ASC' == Input::query('sort_dir','ASC') )
-    ^
+     	&uarr;
     @else
-    {{ HTML::link($sort_url_asc,'^', array(), Request::secure())}}
+    {{ HTML::link($sort_url_asc,'↑', array(), Request::secure())}}
     @endif
 
     @if($attribute == Input::query('sort_by',false) && 'DESC' == Input::query('sort_dir','ASC'))
-    v
+    &darr;
     @else
-    {{ HTML::link($sort_url_desc,'v', array(), Request::secure())}}
+    {{ HTML::link($sort_url_desc,'↓', array(), Request::secure())}}
     @endif
  @endif
