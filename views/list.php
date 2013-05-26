fa3cd0edce6e115de1593facd24b9a48b842056d
Tafhim Ul Islam <tafhim.ul.islam@gmail.com>
2013-05-26 19:17:12 +0600

    Filter GUI form stripped off of Former dependency in the default view. So far, so good

--- views/list.php
+++ views/list.php
@@ -1,32 +1,25 @@
 <?php
-/* Autolist form start */
 if (count($filter_fields) > 0) {
-    //Former::framework('Nude');
-    //echo Former::open()->class('form-search')->method('get')->action('')->id('autolist-filter-form');
     ?>
-<form class="form-search" method="GET" action="" id="autolist-filter-form">
-    <select class="input-medium search-query" id="autolist-filter-by" name="filter_by">
-        <option value="" selected="selected">Select</option>
-        <?php foreach ($filter_fields as $field): ?>
-        <option value="<?php echo e($field['attribute'])?>" data-filter-type='<?php echo json_encode($field['filter_type'])?>'><?php echo e($field['title'])?></option>
-        <?php endforeach; ?>
-    </select>
-    <select class="input-medium search-query" id="autolist-filter-op" name="filter_op" data-filter-ops='<?php echo json_encode($filter_opmap)?>' data-filter-optitles='<?php echo json_encode($filter_optitles)?>'>
-        <option value="" selected="selected">Select</option>
-        <?php foreach ($filter_opmap[$filter_fields['venue_name']['filter_type']] as $operator => $widget): ?>
-        <option value="<?php echo e($operator)?>" data-filter-operator='<?php echo json_encode($widget)?>'><?php echo e($filter_optitles[$operator])?></option>
-        <?php endforeach; ?>
-    </select>
-    <span id="autolist-filter-inputs">
-    </span>
-    <button class="btn" type="submit" >Submit</button>
-</form>
+    <form method="GET" action="" id="autolist-filter-form">
+        <select id="autolist-filter-by" name="filter_by">
+            <option value="" selected="selected">Select</option>
+            <?php foreach ($filter_fields as $field): ?>
+            <option value="<?php echo e($field['attribute'])?>" data-filter-type='<?php echo json_encode($field['filter_type'])?>'><?php echo e($field['title'])?></option>
+            <?php endforeach; ?>
+        </select>
+        <select id="autolist-filter-op" name="filter_op" data-filter-ops='<?php echo json_encode($filter_opmap)?>' data-filter-optitles='<?php echo json_encode($filter_optitles)?>'>
+            <option value="" selected="selected">Select</option>
+            <?php foreach ($filter_opmap[$filter_fields['venue_name']['filter_type']] as $operator => $widget): ?>
+            <option value="<?php echo e($operator)?>" data-filter-operator='<?php echo json_encode($widget)?>'><?php echo e($filter_optitles[$operator])?></option>
+            <?php endforeach; ?>
+        </select>
+        <span id="autolist-filter-inputs">
+        </span>
+        <button type="submit" >Submit</button>
+    </form>
     <?php
-    //echo Former::button('Search')->class('btn')->type('submit');
-    //echo Former::close();
-    //Former::framework('TwitterBootstrap');
 }
-/* Autolist form end */
 ?>
 <table class="autolist">
     <caption><?php echo $title ; ?> </caption>
